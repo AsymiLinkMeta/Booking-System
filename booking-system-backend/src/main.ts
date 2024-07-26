62d22bf6bb0f2fae1fd1c40080004788b77b804b
chrispsang <christinapsang@gmail.com>
2024-07-26 21:27:35 +0400

    Update CORS configuration to allow requests from deployed frontend

--- booking-system-backend/src/main.ts
+++ booking-system-backend/src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: ['http://localhost:3001', 'https://booking-system-muqm.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+    credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
